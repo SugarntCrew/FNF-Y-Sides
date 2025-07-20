f91fbb7233cfe45cc08c426ba189a526fc53d5a3
MrMadera <ismaelglhs@gmail.com>
2025-07-20 11:47:42 +0100

    music gallery basic

--- source/states/GalleryState.hx
+++ source/states/GalleryState.hx
@@ -568,8 +568,195 @@ class GalleryStateImages extends MusicBeatState
 
 class GalleryStateMusic extends MusicBeatState
 {
+    var bg:FlxSprite;
+    var barLeft:FlxSprite;
+    var barRight:FlxSprite;
+    var lines:FlxBackdrop = new FlxBackdrop(Paths.image('gallery/lines'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else XY #end);
+	var diskIconsLeft:FlxBackdrop = new FlxBackdrop(Paths.image('gallery/music/disk'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y, 0, 12 #end);
+	var diskIconsRight:FlxBackdrop = new FlxBackdrop(Paths.image('gallery/music/disk'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y, 0, 12 #end);
+
+    var panel:FlxSprite;
+    var bf:FlxSprite;
+    var arrowUp:FlxSprite;
+    var arrowDown:FlxSprite;
+
+	var wiggle:WiggleEffect = null;
+	var wiggleBg:WiggleEffect = null;
+
+    var musicSongsGrp:FlxTypedGroup<GalleryMusicObject>;
+    var musicSongsArray:Array<String> = [
+        'tutorial',
+        'bopeebo',
+        'fresh',
+        'dad-battle',
+    ];
+
     public function new()
     {
         super();
+
+		wiggle = new WiggleEffect(2, 4, 0.002, WiggleEffectType.DREAMY);
+		wiggleBg = new WiggleEffect(2, 4, 0.002, WiggleEffectType.DREAMY);
+
+		var colorBg:FlxSprite = new FlxSprite().makeGraphic(1280, 720, 0xFFBFB4F1);
+		add(colorBg);
+        
+        bg = new FlxSprite();
+        bg.loadGraphic(Paths.image('gallery/bg'));
+        bg.antialiasing = ClientPrefs.data.antialiasing;
+        bg.shader = wiggleBg;
+        add(bg);
+
+        lines.velocity.set(75, 75);
+        lines.alpha = 0.45;
+        lines.antialiasing = ClientPrefs.data.antialiasing;
+        lines.setPosition(GalleryState.linesPos[0], GalleryState.linesPos[1]);
+        add(lines);
+
+        barLeft = new FlxSprite();
+        barLeft.loadGraphic(Paths.image('gallery/bars_vertical'));
+        barLeft.antialiasing = ClientPrefs.data.antialiasing;
+        add(barLeft);
+
+        barRight = new FlxSprite();
+        barRight.loadGraphic(Paths.image('gallery/bars_vertical'));
+        barRight.antialiasing = ClientPrefs.data.antialiasing;
+        barRight.flipX = true;
+        barRight.x = FlxG.width - barRight.width;
+        add(barRight);
+
+        diskIconsLeft.velocity.set(0, -50);
+        diskIconsLeft.antialiasing = ClientPrefs.data.antialiasing;
+        diskIconsLeft.x = barLeft.x + (barLeft.width / 2) - (diskIconsLeft.width / 2) - (17 / 2);
+		add(diskIconsLeft);
+
+        diskIconsRight.flipX = true;
+        diskIconsRight.velocity.set(0, 50);
+        diskIconsRight.y = FlxG.height - diskIconsRight.height;
+        diskIconsRight.antialiasing = ClientPrefs.data.antialiasing;
+        diskIconsRight.x = barRight.x + (barRight.width / 2) - (diskIconsRight.width / 2) + (17 / 2);
+        add(diskIconsRight);
+
+        panel = new FlxSprite(775, 0);
+        panel.loadGraphic(Paths.image('gallery/music/panel'));
+        panel.antialiasing = ClientPrefs.data.antialiasing;
+        panel.screenCenter(Y);
+        add(panel);
+
+        bf = new FlxSprite(0, 0);
+        bf.loadGraphic(Paths.image('gallery/music/bf'));
+        bf.antialiasing = ClientPrefs.data.antialiasing;
+        bf.screenCenter(Y);
+        bf.x = panel.x + (panel.width / 2) - (bf.width / 2) - 15;
+        bf.y = panel.y + (panel.height / 2) - (bf.height / 2) - 15;
+        add(bf);
+
+        musicSongsGrp = new FlxTypedGroup<GalleryMusicObject>();
+        add(musicSongsGrp);
+
+        arrowUp = new FlxSprite(355, 130);
+        arrowUp.loadGraphic(Paths.image('gallery/music/arrow'));
+        arrowUp.antialiasing = ClientPrefs.data.antialiasing;
+        add(arrowUp);
+
+        arrowDown = new FlxSprite(355, 530);
+        arrowDown.loadGraphic(Paths.image('gallery/music/arrow'));
+        arrowDown.antialiasing = ClientPrefs.data.antialiasing;
+        arrowDown.flipY = true;
+        add(arrowDown);
+
+        for(i in 0...musicSongsArray.length)
+        {
+            var spr = new GalleryMusicObject();
+		    spr.loadGraphic(Paths.image('songCards/${Paths.formatToSongPath(musicSongsArray[i])}'));
+            spr.antialiasing = ClientPrefs.data.antialiasing;
+            spr.screenCenter(Y);
+            spr.x = arrowUp.x + (arrowUp.width / 2) - (spr.width / 2);
+            spr.startPosition = new FlxPoint(spr.x, spr.y);
+            spr.targetY = i;
+            spr.y += FlxG.height * i;
+            spr.shader = wiggle;
+            musicSongsGrp.add(spr);
+
+            var ogY:Float = spr.y;
+            spr.y = 800;
+            FlxTween.tween(spr, {y: ogY - 10}, 0.8, {ease: FlxEase.quartOut, startDelay: 0.1, onComplete: function(t:FlxTween)
+            {
+                FlxTween.tween(spr, {y: spr.y + 20}, 7, {ease: FlxEase.quartInOut, type: PINGPONG});
+            }});
+        }
+    }
+
+    var alreadyPressedSmth:Bool = false;
+    var updateArrowScale:Bool = true;
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+		if (wiggle != null) {
+			wiggle.update(elapsed);
+		}
+
+		if (wiggleBg != null) {
+			wiggleBg.update(elapsed);
+		}
+
+        var upMult:Float = FlxMath.lerp(arrowUp.scale.x, 1, elapsed * 9);
+        if(updateArrowScale) arrowUp.scale.set(upMult, upMult);
+
+        var rightMult:Float = FlxMath.lerp(arrowDown.scale.x, 1, elapsed * 9);
+        if(updateArrowScale) arrowDown.scale.set(rightMult, rightMult);
+
+        if(!alreadyPressedSmth)
+        {
+            if(controls.UI_DOWN_P)
+            {
+                changeSelect(1);
+                arrowDown.scale.set(0.9, 0.85);
+            }
+            if(controls.UI_UP_P)
+            {
+                changeSelect(-1);
+                arrowUp.scale.set(0.9, 0.85);
+            }
+
+            if(controls.BACK)
+            {
+                MusicBeatState.switchState(new GalleryState());
+            }
+        }
+    }
+    
+    private static var curSelected:Int = 0;
+    function changeSelect(change:Int = 0)
+    {
+        curSelected = FlxMath.wrap(curSelected + change, 0, musicSongsGrp.length - 1);
+
+		for (num => item in musicSongsGrp.members)
+		{
+			item.targetY = num - curSelected;
+			item.alpha = 0.6;
+			if (item.targetY == 0) item.alpha = 1;
+		}
+    }
+}
+
+class GalleryMusicObject extends FlxSprite
+{
+    public var targetY:Float = 0;
+    public var distancePerItem:FlxPoint = new FlxPoint(0, 720);
+    public var startPosition:FlxPoint = new FlxPoint(0, 0);
+    
+    public function new(x:Float = 0, y:Float = 0, ?graphic:Dynamic = null)
+    {
+        super(x, y, graphic);
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+		var lerpVal:Float = Math.exp(-elapsed * 9.6);
+		y = FlxMath.lerp((targetY * distancePerItem.y) + startPosition.y, y, lerpVal);
     }
 }
