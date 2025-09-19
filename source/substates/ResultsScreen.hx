7b8dbe276b156d08fda66ffc04478b2d7bf10701
MrMadera <ismaelglhs@gmail.com>
2025-09-19 16:14:10 +0100

    results screen progress

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -15,16 +15,42 @@ class ResultsScreen extends MusicBeatSubstate
         bg.makeGraphic(FlxG.width, FlxG.height, 0xFFCFC6F3);
         add(bg);
 
+        var bgStripe = new FlxSprite();
+        bgStripe.loadGraphic(Paths.image('resultsScreen/newResultsScreen/stripe'));
+        bgStripe.antialiasing = ClientPrefs.data.antialiasing;
+        bgStripe.blend = ADD;
+        add(bgStripe);
+
+        var bfIconLeft = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/icon'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y #end);
+        bfIconLeft.velocity.set(0, -10);
+        bfIconLeft.antialiasing = ClientPrefs.data.antialiasing;
+        add(bfIconLeft);
+        
+        var bfIconRight = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/icon'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y #end);
+        bfIconRight.x = FlxG.width - bfIconRight.width;
+        bfIconRight.velocity.set(0, 10);
+        bfIconRight.antialiasing = ClientPrefs.data.antialiasing;
+        add(bfIconRight);
+
         var patternDown = new ResultsScreenPattern(0, 0);
         patternDown.y = FlxG.height - patternDown.height;
         patternDown.velocity.set(10, 0);
+        patternDown.antialiasing = ClientPrefs.data.antialiasing;
         add(patternDown);
 
         var patternUp = new ResultsScreenPattern(0, 0, true);
         patternUp.y = 0;
         patternUp.flipY = true;
         patternUp.velocity.set(-10, 0);
+        patternUp.antialiasing = ClientPrefs.data.antialiasing;
         add(patternUp);
+
+        var board = new FlxSprite();
+        board.loadGraphic(Paths.image('resultsScreen/newResultsScreen/board'));
+        board.screenCenter();
+        board.x += FlxG.width / 6;
+        board.antialiasing = ClientPrefs.data.antialiasing;
+        add(board);
     }
 
     override function update(elapsed:Float)
@@ -62,10 +88,12 @@ class ResultsScreenPattern extends FlxSpriteGroup
         super(x, y);
 
         darkPattern = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/lettaBoxDark'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else X #end);
+        darkPattern.antialiasing = ClientPrefs.data.antialiasing;
         add(darkPattern);
 
         lightPattern = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/lettaBoxLight'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else X #end);
         lightPattern.y = flipPatternY ? darkPattern.y : darkPattern.y + darkPattern.height - lightPattern.height;
+        lightPattern.antialiasing = ClientPrefs.data.antialiasing;
         add(lightPattern);
     }
 }
